5540d2b8780b8391ba32926119bdd7ca7f250676
Max <210164751+MaxTheFaul@users.noreply.github.com>
2025-08-11 17:39:13 +0200

    Add Human playing system

--- js.js
+++ js.js
@@ -1,13 +1,27 @@
+let roundsPlayed = 0;
+let scoreConputer = 0;
+let scoreHuman = 0;
 function getComputerChoice() {
   number = Math.random();
   console.log(number);
   if (number < 0.33) {
     return choice = "Schere";
   }
-  if (number > 0.33 && number < 0.66) {
+  else if (number > 0.33 && number < 0.66) {
     return choice = "Stein";
   } else {
     return choice = "Papier";
   }
 }
-console.log(getComputerChoice());
+function getPlayerChoice(){
+    const Choice = prompt("Schere, Stein oder Papier")
+    if (Choice.toLowerCase() === "schere") {
+        return playerChoice = "Schere"
+    }else if (Choice.toLowerCase() === "stein") {
+        return playerChoice = "Stein"
+    } 
+    else {
+        return playerChoice = "papier"
+    }
+    
+}
