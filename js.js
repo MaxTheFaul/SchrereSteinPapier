6c80e278e93712daed5d5c3fdde8b7d8b73d3cc2
Max <210164751+MaxTheFaul@users.noreply.github.com>
2025-10-05 10:19:07 +0200

    create ui for playing the game

--- js.js
+++ js.js
@@ -3,9 +3,11 @@ let scoreConputer = 0;
 let scoreHuman = 0;
 const winMessage = "Congratulation, you won";
 const loseMessage = "You Lost, good luck next time";
+const sect = document.querySelector(".section");
+console.log(sect);
 function getComputerChoice() {
   number = Math.random();
-  console.log(number);
+  logMessage(number);
   if (number < 0.33) {
     return "Schere";
   } else if (number > 0.33 && number < 0.66) {
@@ -14,57 +16,60 @@ function getComputerChoice() {
     return "Papier";
   }
 }
-function getPlayerChoice() {
-  const Choice = prompt("Schere, Stein oder Papier");
-  if (Choice.toLowerCase() === "schere") {
-    return "Schere";
-  } else if (Choice.toLowerCase() === "stein") {
-    return "Stein";
-  } else {
-    return "Papier";
-  }
-}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playRound(getComputerChoice(), button.id);
+  });
+});
+
 function playRound(computerChoice, humanChoice) {
   roundsPlayed++;
   if (computerChoice === humanChoice) {
-    console.log("tie");
+    logMessage("tie");
   } else {
     if (computerChoice === "Stein" && humanChoice === "Schere") {
-      console.log(loseMessage);
+      logMessage(loseMessage);
       scoreConputer++;
     } else if (computerChoice === "Stein" && humanChoice === "Papier") {
-      console.log(winMessage);
+      logMessage(winMessage);
       scoreHuman++;
     } else if (computerChoice === "Papier" && humanChoice === "Schere") {
-      console.log(winMessage);
+      logMessage(winMessage);
       scoreHuman++;
     } else if (computerChoice === "Papier" && humanChoice === "Stein") {
-      console.log(loseMessage);
+      logMessage(loseMessage);
       scoreConputer++;
     } else if (computerChoice === "Schere" && humanChoice === "Papier") {
-      console.log(loseMessage);
+      logMessage(loseMessage);
       scoreConputer++;
     } else {
-      console.log(winMessage);
+      logMessage(winMessage);
       scoreHuman++;
     }
   }
-}
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    let computerSelection = getComputerChoice();
-    let humanSelection = getPlayerChoice();
-    playRound(computerSelection, humanSelection);
-    console.log(roundsPlayed);
+  if (roundsPlayed >= 5) {
+    winChoice();
   }
-  console.log("Computer Score " + scoreConputer);
-  console.log("Your Score " + scoreHuman);
+}
+function winChoice() {
+  logMessage("Computer Score " + scoreConputer);
+  logMessage("Your Score " + scoreHuman);
   if (scoreHuman > scoreConputer) {
-    console.log(winMessage);
+    logMessage(winMessage);
   } else if (scoreConputer === scoreHuman) {
-    console.log("tie");
+    logMessage("tie");
   } else {
-    console.log(loseMessage);
+    logMessage(loseMessage);
   }
 }
-playGame();
+function logMessage(message) {
+  const para = document.createElement("p");
+  para.textContent = message;
+  sect.appendChild(para);
+  const txt = document.createElement("p");
+  txt.textContent =
+    "----------------------------------------------------------------------------";
+  sect.appendChild(txt);
+}
